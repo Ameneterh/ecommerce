4f88166257f178f4fb94229532292fd975d9da3a
Amene Terhemen <ameneterh@gmail.com>
2024-12-30 15:03:21 +0100

    updated index.js file

--- backend/index.js
+++ backend/index.js
@@ -31,9 +31,9 @@ const __dirname = path.resolve();
 
 // render deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
