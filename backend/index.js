098049f27e302d770debd256fc425b859a76ec80
Amene Terhemen <ameneterh@gmail.com>
2024-12-30 15:08:39 +0100

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,19 @@ import productRouter from "./routes/product.route.js";
 import bidsRouter from "./routes/bids.routes.js";
 import notificationsRouter from "./routes/notification.route.js";
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import path from "path";
-import cors from "cors";
+
+dotenv.config();
+
+// deployment config
+const __dirname = path.resolve();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -18,16 +24,10 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cookieParser());
-
 app.use("/backend/users", userRouter);
 app.use("/backend/products", productRouter);
 app.use("/backend/bids", bidsRouter);
 app.use("/backend/notifications", notificationsRouter);
-
-// deployment config
-const __dirname = path.resolve();
 
 // render deployment
 if (process.env.NODE_ENV === "production") {
